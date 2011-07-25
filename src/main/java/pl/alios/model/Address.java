aa39dcf528211398d5ed9fba2e2d292e9fe2c73d
daniel.klosinski <daniel.klosinski@gmail.com>
2011-07-25 18:21:19 +0000

    Konfiguracja

--- src/main/java/pl/alios/model/Address.java
+++ src/main/java/pl/alios/model/Address.java
@@ -4,10 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
-import pl.alios.AbstarctBean;
 
 @Entity
 public class Address {

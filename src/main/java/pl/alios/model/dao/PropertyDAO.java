00337ba8dbd2d294bd895a9f2d0fabb0f8284165
daniel.klosinski <daniel.klosinski@gmail.com>
2011-08-01 01:08:24 +0000

    Numer klienta generowany od 1000

--- src/main/java/pl/alios/model/dao/PropertyDAO.java
+++ src/main/java/pl/alios/model/dao/PropertyDAO.java
@@ -1,12 +1,18 @@
 package pl.alios.model.dao;
 
+import javax.persistence.Query;
+
+import pl.alios.model.Property;
+
 public class PropertyDAO extends AbstarctDAO{
 
-	public String getValue(String name){
-		
-		return "";
+	public Property getValue(String name){
+		logger.info("SELECT p FROM pl.alios.model.Property p WHERE p.name = :name");
+		Query query = em.createQuery("SELECT p FROM pl.alios.model.Property p WHERE p.name = :name");
+		query.setParameter("name", name);
+		Property property = (Property) query.getSingleResult();
+		return property;
 	}
-	
 	
 	
 	
--- src/main/java/pl/alios/model/dao/adapter/DBAdapter.java
+++ src/main/java/pl/alios/model/dao/adapter/DBAdapter.java
@@ -4,6 +4,7 @@ import pl.alios.model.dao.CategoryDAO;
 import pl.alios.model.dao.CustomerDAO;
 import pl.alios.model.dao.OrderDAO;
 import pl.alios.model.dao.ProductDAO;
+import pl.alios.model.dao.PropertyDAO;
 
 public class DBAdapter {
 	
@@ -11,21 +12,26 @@ public class DBAdapter {
 	private ProductDAO productDAO;
 	private OrderDAO orderDAO;
 	private CategoryDAO categoryDAO;
+	private PropertyDAO propertyDAO;
     
     private DBAdapter() {
     	customerDao = new CustomerDAO();
     	productDAO = new ProductDAO();
     	orderDAO = new OrderDAO();
     	categoryDAO = new CategoryDAO();
+    	propertyDAO = new PropertyDAO();
     }
 	
 	public CustomerDAO getCustomerDao() {return customerDao;}
     public ProductDAO getProductDAO() {return productDAO;}
 	public OrderDAO getOrderDAO() {return orderDAO;}
 	public CategoryDAO getCategoryDAO() {return categoryDAO;}
+	public PropertyDAO getPropertyDAO() {return propertyDAO;}	
 	
 	
 	//------------- SINGLETON ------------------------
+
+
 
 
 	private static final DBAdapter INSTANCE = new DBAdapter();

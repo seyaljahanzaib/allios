823e9ce030643e25b88e913d542c2f6f7bfe81e0
daniel.klosinski <daniel.klosinski@gmail.com>
2011-08-01 00:22:00 +0000

    Podpięcie mapowań dla

--- src/main/java/pl/alios/main/LinkAction.java
+++ src/main/java/pl/alios/main/LinkAction.java
@@ -12,18 +12,32 @@ public class LinkAction extends ActionSupport {
 	private static final long serialVersionUID = -2613425890762568273L;
 
 	
-	public String welcome()
-	{
-		return "welcome";		
-	}
-	public String register()
-	{
-		return "register";		
-	}
-	public String logon()
-	{
-		return "have_to_log_account";		
-	}
+	public String welcome(){return "welcome";}
+	
+	public String register(){return "register";}
+	
+	public String logon(){return "have_to_log_account";}
+	
+	public String friends(){return "friends";}
+	
+	public String office(){return "office";}
+	
+	public String orderReview(){return "order_review";}
+	
+	public String changeData(){return "changeData";}
+	
+	public String changePass(){return "changePass";}
+	
+	public String mainPage(){return "mainPage";}	
+	
+	public String statute(){return "statute";}		
+	
+	public String aboutUs(){return "aboutUs";}		
+	
+	public String contact(){return "contact";}		
+		
+	public String news(){return "news";}		
+	
 	public String cart()
 	{
 		Map<String,Object> sessionAttr = ActionContext.getContext().getSession();
@@ -33,26 +47,7 @@ public class LinkAction extends ActionSupport {
 		}
 		return "cart";		
 	}
-	public String friends()
-	{
-		return "friends";		
-	}
-	public String office()
-	{
-		return "office";		
-	}
-	public String orderReview()
-	{
-		return "order_review";		
-	}
-	public String changeData()
-	{
-		return "changeData";		
-	}
-	public String changePass()
-	{
-		return "changePass";		
-	}
+
 	
 	
 	

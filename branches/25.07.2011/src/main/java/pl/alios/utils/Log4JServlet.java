d246da3b432f8529f742e3ed3aae06442f675e59
daniel.klosinski <daniel.klosinski@gmail.com>
2011-08-29 01:35:43 +0000

    Poprawka w wyswietlaniu produktow w kategoriach

--- branches/25.07.2011/src/main/java/pl/alios/utils/Log4JServlet.java
+++ branches/25.07.2011/src/main/java/pl/alios/utils/Log4JServlet.java
@@ -58,7 +58,6 @@ public class Log4JServlet extends HttpServlet {
 		Map<String, ArrayList<Product>> productMap = new HashMap<String, ArrayList<Product>>();
 		for(Product product: products){
 			if(product.getCategory().getActive().equals(Boolean.FALSE)) continue;
-			
 			if(productMap.get(product.getCategory().getId().toString()) != null){
 				productMap.get(product.getCategory().getId().toString()).add(product);
 				
@@ -70,6 +69,10 @@ public class Log4JServlet extends HttpServlet {
 			if(product.getCategory() != null && product.getCategory().getMainCategory() != null){
 				if(productMap.get(product.getCategory().getMainCategory().getId().toString()) != null){
 					productMap.get(product.getCategory().getMainCategory().getId().toString()).add(product);
+				}else{
+					ArrayList<Product> newList2 = new ArrayList<Product>();
+					newList2.add(product);
+					productMap.put(product.getCategory().getMainCategory().getId().toString(), newList2);
 				}
 			}
 		}
